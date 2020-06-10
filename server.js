403171cef83c42c3e400c189f747c4da5c8848cc
ebinxavier <ebinx7@gmail.com>
2020-06-10 20:33:07 +0400

    port fix.

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-
-app.listen(4000,()=>{console.log("Listening to 4000...!")})
+const port = process.env.PORT || 4000;
+app.listen(port,()=>{console.log("Listening to 4000...!")})
 
 app.use('/room',room);
 app.use('/message',message);
